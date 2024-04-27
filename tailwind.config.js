263dee4ba7f68ac11c7e05f36511a1d8dd24f00d
kushan-ghughu <kushanghughu25@gmail.com>
2024-04-27 11:25:25 +0530

    finish the teams part

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,13 @@ export default {
         lg:"1.25rem",
         xl:"1.5rem",
         "2xl":"1.75rem",
-        "3xl":"1.25rem",
-      }
+        "3xl":"2.25rem",
+      },
+
+      backgroundImage: {
+        'team-bg-image': "url('/public/teams/team.png')", 
+      }
+
     },
   },
   plugins: [],
